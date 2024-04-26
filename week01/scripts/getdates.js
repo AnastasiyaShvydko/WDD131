31db97565c8a93f378c2fb70f4461252f4643dc0
AnastasiyaShvydko <84057111+AnastasiyaShvydko@users.noreply.github.com>
2024-04-26 13:40:05 -0400

    week1

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,9 @@
+
+
+let currentYear = new Date().getFullYear();
+let date_element = document.getElementById('currentyear');
+date_element.innerHTML = currentYear;
+
+let lastModified = document.lastModified;
+let lastModified_element = document.getElementById('lastModified');
+lastModified_element.innerHTML = lastModified;
