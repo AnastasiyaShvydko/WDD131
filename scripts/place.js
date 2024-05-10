4fe286243becfed8a4a4be7cd971d15d188f588a
AnastasiyaShvydko <84057111+AnastasiyaShvydko@users.noreply.github.com>
2024-05-09 23:03:06 -0400

    week3

--- scripts/place.js
+++ scripts/place.js
@@ -1,7 +1,8 @@
-const AirTemperature = parseInt(document.getElementById('temperature').value);
-const WindSpeed = parseInt(document.getElementById('wind').value);
+const AirTemperature = document.getElementById('temperature').innerHTML;
+const WindSpeed = document.getElementById('wind').innerHTML;
 let windChillValue = document.getElementById('wind-chill');
-
+console.log(AirTemperature);
+console.log(WindSpeed);
 const findWindChill = (temp, speed) =>{
     let windChill = 13.12 + 0.6215*temp - 11.37*Math.pow(speed,0.16)+0.3965*temp*Math.pow(speed,0.16);
     return windChill;
@@ -9,9 +10,9 @@ const findWindChill = (temp, speed) =>{
 
 if(AirTemperature <= 10 && WindSpeed >= 4.8){
 
-    windChillValue.value = `${findWindChill(AirTemperature, WindSpeed).toFixed(2)}`;
+    windChillValue.innerHTML = `${findWindChill(AirTemperature, WindSpeed).toFixed(2)}`;
 
 }
 else{
-    windChillValue.value = "N/A";
+    windChillValue.innerHTML = "N/A";
 }
